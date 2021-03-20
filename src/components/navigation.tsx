4335ed4fc93281b595e904057121e89ec3e432d0
Ahad Chowdhury <ahad@maacpiash.com>
2021-03-20 16:36:24 +1100

    Add logo (transparent bg) and fix its styling

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -87,7 +87,7 @@ const Navbar: React.FC<NavbarProps> = ({ navPlaceholder, location, currentTheme,
                 <Link to="/" title={data.site.siteMetadata.title}>
                     <Logo
                         className={`duration-300 transition-all ${
-                            scrolled ? "w-6" : "w-8"
+                            scrolled ? "w-12" : "w-16"
                         }`}
                     />
                 </Link>
